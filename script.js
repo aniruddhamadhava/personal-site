2f3cebbaa1241ace279458718fa9ceaea3f8c2a4
Aniruddha M <106787581+aniruddhamadhava@users.noreply.github.com>
2023-12-28 14:44:49 -0500

    script.js

--- script.js
+++ script.js
@@ -27,3 +27,15 @@
   });
   
 })(jQuery);
+
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.text');
+
+readMoreBtn.addEventListener('click', (e) => {
+    text.classList.toggle('show-more');
+    if(readMoreBtn.innerText === 'Read More') {
+        readMoreBtn.innerText = 'Read Less';
+    } else {
+        readMoreBtn.innerText = 'Read More';
+    }
+})
